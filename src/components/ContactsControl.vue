f6bf9912fde2d8bbadb3ac401feab7891f473a6a
Roy Mosby <ego.madking@gmail.com>
2019-10-31 03:29:44 -0400

    ContactsFilter.vue rename

---
+++ src/components/ContactsControl.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>contacts control component</p>
+    <SearchAndSort />
+    <CreateNew />
+  </div>
+</template>
+
+<script>
+import CreateNew from '@/components/CreateNew.vue';
+import SearchAndSort from '@/components/SearchAndSort.vue';
+export default {
+  components: {
+    CreateNew,
+    SearchAndSort
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
