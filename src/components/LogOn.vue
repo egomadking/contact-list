63527039a337676edc42e8180bfea9801188439d
Roy Mosby <ego.madking@gmail.com>
2019-10-31 03:31:22 -0400

    dummy login added

--- src/components/LogOn.vue
+++ src/components/LogOn.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="login-wrapper">
     <img :src="require('@/assets/phone.svg')" />
-    <form action="" method="get">
+    <h2>
+      Login does not do anything yet. Contact changes do not persist either.
+    </h2>
+    <form
+      id="loginForm"
+      @submit.prevent="submitLoginDetails"
+      action=""
+      method="get"
+    >
       <label for="username">User Name</label>
-      <input type="text" id="username" />
+      <input type="text" id="username" :value="username" />
 
       <label for="password">Password</label>
-      <input type="password" id="password" />
+      <input type="password" id="password" :value="password" />
+
+      <input type="submit" value="Submit" />
     </form>
   </div>
 </template>
@@ -14,8 +24,20 @@
 <script>
 import LoginService from '@/services/LoginService.js';
 export default {
+  data() {
+    return {
+      username: 'Moose',
+      password: ''
+    };
+  },
   created() {
     LoginService.helloWorld();
+  },
+  methods: {
+    submitLoginDetails() {
+      console.log('doing login stuff');
+      this.$router.push({ path: '/' });
+    }
   }
 };
 </script>
@@ -30,6 +52,12 @@ form {
   width: 450px;
   & label {
     text-align: left;
+  }
+  & input[type='submit'] {
+    background-color: #deedfc;
+    color: #336699;
+    font-size: 1.4em;
+    grid-column: 1/3;
   }
 }
 img {
