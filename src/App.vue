ea3622666f10b5d6b15c1ea5f925beac28b6b5c6
Roy Mosby <roy.e.mosby@gmail.com>
2019-11-01 21:01:15 -0500

    fixed link partion spacing, added global nav and link styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Browse</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/add">Add</router-link> |
-      <router-link to="/edit">Edit</router-link> |
+    <nav>
+      <router-link to="/">Browse</router-link>
+      <span>|</span>
+      <router-link to="/about">About</router-link>
+      <span>|</span>
+      <router-link to="/add">Add</router-link>
+      <span>|</span>
+      <router-link to="/edit">Edit</router-link>
+      <span>|</span>
       <router-link to="/login">Login</router-link>
-    </div>
+    </nav>
     <router-view />
   </div>
 </template>
@@ -22,5 +26,33 @@
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+nav {
+  & a {
+    font-weight: 600;
+  }
+  & span {
+    display: inline-block;
+    padding: 0 0.5em;
+  }
+}
+select,
+input {
+  background-color: #ddd;
+  border: none;
+  padding: 0;
+  right: 0;
+}
+button:focus,
+input:focus,
+select:focus {
+  outline: 1px solid #336699;
+}
+
+.router-link-active.router-link-exact-active {
+  color: #336699;
+}
+a:visited {
+  color: #6a86a1;
 }
 </style>
